5f55938687670130ff8ce072874a1d728bafd23e
Farhan Rehman <37966672+FarhanRehman@users.noreply.github.com>
2021-01-02 20:17:09 -0500

    Update getData.py

--- SentimentAnalysis/Reddit/Data Processing/getData.py
+++ SentimentAnalysis/Reddit/Data Processing/getData.py
@@ -4,7 +4,6 @@
 from RedditScraper import RedditScraper 
 from nltk import tokenize
 import yfinance as yf
-import requests
 import timeit
 import csv
 import re
@@ -32,26 +31,60 @@ class GetData:
     def valtiadeTickers(self, tickers):
         valid = set()
         for ticker in tickers:
-            if ticker in self.blacklistTickers:
-                tickers.remove(ticker)
-            else:
+            if ticker not in self.blacklistTickers:
                 # search csv file for ticker to check if it is valid
                 with open('C:/Users/farha/Documents/GitHub/SelectMarket/SentimentAnalysis/Reddit/STOCKS.csv', 'rt') as csvfile:
                     my_content = csv.reader(csvfile, delimiter=',')
                     for row in my_content:
                         if ticker in row:
                             valid.add(ticker)
- 
-                # response = requests.get(f'http://finance.yahoo.com/quote/{ticker}')
-                # if len(response.history) == 1:
-                #     valid.add(ticker)
 
         return valid
 
     # TODO: sentiment analysis comment with many tickers
+    # TODO: return dict with cleaned data as well so sentiment analysis can be Preformed
+
+
+    def split_into_sentences(self, text):
+        alphabets= "([A-Za-z])"
+        prefixes = "(Mr|St|Mrs|Ms|Dr)[.]"
+        suffixes = "(Inc|Ltd|Jr|Sr|Co)"
+        starters = "(Mr|Mrs|Ms|Dr|He\s|She\s|It\s|They\s|Their\s|Our\s|We\s|But\s|However\s|That\s|This\s|Wherever)"
+        acronyms = "([A-Z][.][A-Z][.](?:[A-Z][.])?)"
+        websites = "[.](com|net|org|io|gov)"
+
+        text = " " + text + "  "
+        text = text.replace("\n"," ")
+        text = re.sub(prefixes,"\\1<prd>",text)
+        text = re.sub(websites,"<prd>\\1",text)
+        if "Ph.D" in text: text = text.replace("Ph.D.","Ph<prd>D<prd>")
+        text = re.sub("\s" + alphabets + "[.] "," \\1<prd> ",text)
+        text = re.sub(acronyms+" "+starters,"\\1<stop> \\2",text)
+        text = re.sub(alphabets + "[.]" + alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>\\3<prd>",text)
+        text = re.sub(alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>",text)
+        text = re.sub(" "+suffixes+"[.] "+starters," \\1<stop> \\2",text)
+        text = re.sub(" "+suffixes+"[.]"," \\1<prd>",text)
+        text = re.sub(" " + alphabets + "[.]"," \\1<prd>",text)
+        if "”" in text: text = text.replace(".”","”.")
+        if "\"" in text: text = text.replace(".\"","\".")
+        if "!" in text: text = text.replace("!\"","\"!")
+        if "?" in text: text = text.replace("?\"","\"?")
+        text = text.replace(".",".<stop>")
+        text = text.replace("?","?<stop>")
+        text = text.replace("!","!<stop>")
+        text = text.replace("<prd>",".")
+        sentences = text.split("<stop>")
+        sentences = sentences[:-1]
+        sentences = [s.strip() for s in sentences]
+        return sentences
 
     # This function wil parse all tickers and related text from a submission/comment, and output it in a dictionary/csv
     def parseTickers(self, text):
+        # GET CLEANED DATA HERE
+        
+        text = text.replace('\n','')
+        text = text.replace('\t','')
+        
         # Regex to identify stock tickers, we use a set here to avoid duplicates
         tickers = set(re.findall("(?:(?<=\A)|(?<=\s)|(?<=[$]))([A-Z]{1,5})(?=\s|$|[^a-zA-z])", text))
 
@@ -65,9 +98,19 @@ class GetData:
         if len(tickers) == 1:
             tickers = {tickers[0]: text}
         else:
-            # seperate tickers based on sentences
-            sentences = tokenize.sent_tokenize(text)
 
+            # TODO: check out some ways to split into sentences (should seperate by \n as well), check out nltk and regex examples
+            # https://stackoverflow.com/questions/4576077/how-can-i-split-a-text-into-sentences
+
+            # seperate tickers based on sentences
+            # sentences = tokenize.sent_tokenize(text)
+            # print(sentences)
+
+            sentences = self.split_into_sentences(text)
+            print(sentences)
+            # print(sentences)
+
+            # TODO: if ticker appears twice it will overright so find a fix for that
             # assign each ticker the sentence it was included in
             for sentence in sentences:
                 for key in tickers:
@@ -77,18 +120,61 @@ class GetData:
 
 
 r = GetData("WallStreetBets")
-r.parseTickers("""Thinking BABA, BIDU, BABA, TCEHY, YY, IQ have decent chance to outgrow FAANG over next 5 years. Throw in TME and HUYA for a little more 🌙 potential.""")
+r.parseTickers("""
+I know everyone is already holding $GME and $PLTR until they lands on the moon but maybe we can find out what new tickers everyone is holding, why and what are their expectations.
 
 
+Here is a list of my current positions;
 
 
+* 70 shares of $BABA at an average of $245 with a price target of $265, where I'll liquidate unless the political environment gets better.
+
+* COVID proof
+
+* Solid financials, much more liquid than Amazon
+
+* China is bound to become the largest economy
+
+* Political uncertainties are exaggerated
+
+* 70 shares of $CRM at $221 with a price target of $230. Have made this same play quite a few times in the last 2 months.
+
+* COVID proof
+
+* A solid company run by a solid CEO with good growth and financials
+
+* Slack was purchased for $28b but the market has wiped 26% of its market cap off, which is more than $50b so at the very least the overreaction is extended by at least $22b (a 10% increase in market cap from current levels)
+
+* Short-term PT of $230 but have no doubt this can break above $275 in 2021. A 24% upside.
+
+* 24 shares of $FB at $273 with a price target of $280. Have also done this multiple times during the last few months.
+
+* COVID proof
+
+* Earnings coming up near the end of January.
+
+* Zuck might be a lizard, but those IG "influencers" you follow from your fake account so your girlfriend doesn't find out will only keep luring you back in.
+
+* Political risks surrounding breakup are exaggerated and seem to be an optic play to gain public confidence. Even if this is not the case, the first hearing is years away.
+
+* Has the potential to breach $325 during 2021. An upside of 20%.
+
+* 4 shares of $AMZN at $3250 with a price target of $3350 at least. Have made a bit over $5k only trading swings between $3000 and $3350.
+
+* COVID proof
+
+* Earnings coming up near the end of January.
+
+* Possible to reach ATH which is around $3500 near earnings.
+
+* Possible to breach $4000 during the year providing an upside of 23%.
+
+* 275 shares of $PFE at average $38.62 with a price target of $41-42, not sure how this will go. Have made this play once before when I cashed out at $42.75 just a few weeks ago.
+
+* Only speculative play in my portfolio
+
+* Not a hold in the long run as this stock is as limp as they come but a reasonable dividend yield. So, not really worried about losing money.
 
 
-# def validateTicker(self, ticker):
-#     try:
-#         response = yf.Ticker(ticker)
-#         response.info
-#         return True
-#     except:
-#         return False
-
+Pretty boring shit, no $GME or $PLTR here but looking forward to seeing what other tickers the community is holding on to.
+""")
